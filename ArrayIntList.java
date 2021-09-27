31281c535dc315fb70ac13f0dbd061903af918fa
Linus <linushvenfelt@gmail.com>
2021-09-27 16:48:52 +0200

    transfer

--- ArrayIntList.java
+++ ArrayIntList.java
@@ -7,16 +7,15 @@ public class ArrayIntList extends AbstractIntCollection implements IntList {
     public void add(int n) {
         System.out.println("Adding " + n + " Size is: " + this.size);
         
-        for (int i = 0; i < this.size + 1; i++) {
-
+        for (int i = 0; i < this.size + 1; i++) { // TODO
             if (this.values[i] == 0) {
                 //System.out.println(this.values[i] + " = empty space found, adding");
                 this.values[this.size] = n;
                 break;
             } else {
+                // TODO: Make array bigger and add at end
                 //System.out.println(this.values[i] + " = not empty");
             }
-            
         }
         this.size++;
     }
@@ -25,9 +24,37 @@ public class ArrayIntList extends AbstractIntCollection implements IntList {
     /* Inserts integer n at position index. Shifts the element currently at that 
 	 * position (if any) and any subsequent elements to the right.  */
 
-        
+        /**
+         * [ 1 2   3 4 5 ]
+         * [ 1 2 9 3 4 5 ]
+         */
 
-        
+        int[] newarray = new int[this.size + 1];
+        boolean addedN = false;
+
+        for (int i = 0; i < this.size; i++) {
+
+            if (addedN == false) {
+                if (i == index) {
+                    System.out.println("Adding " + n + " at index " + i + " Size is: " + this.size);
+                    newarray[i] = n;
+                    addedN = true;
+                    this.size++;
+                } else {
+                    newarray[i] = this.values[i];
+                }
+            } else {
+                int j = i - 1;
+                newarray[i] = this.values[j];
+            }
+        }
+
+        this.values = newarray;
+
+        /*
+        if (addedN) {
+            this.size; // Todo: TEST
+        }*/
     }
 
     public void remove(int index) throws IndexOutOfBoundsException {
--- CollectionMain.java
+++ CollectionMain.java
@@ -1,10 +1,5 @@
-import two_dv600.IntList;
-
 public class CollectionMain {
     
-
-    
-
     public static void main(String[] args) {
 
         ArrayIntList test = new ArrayIntList();
@@ -12,12 +7,14 @@ public class CollectionMain {
         test.add(1);
         test.add(2);
         test.add(3);
+        test.add(4);
+        test.add(5);
+
+        test.addAt(9, 2);
+
+        test.add(6);
 
         System.out.println(test.toString());
 
-        /*
-        for (int i = 0; i < test.size(); i++) {
-            System.out.println(test.get(i));
-        }*/
     }
 }
