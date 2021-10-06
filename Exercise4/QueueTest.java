204bc8b1d615f0682941e155a2d3179018e74f3e
Linus <linushvenfelt@gmail.com>
2021-10-06 15:18:27 +0200

    Queue

--- Exercise4/QueueTest.java
+++ Exercise4/QueueTest.java
@@ -1,14 +1,54 @@
 package Exercise4;
 
-import java.util.Iterator;
-import java.util.List;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class QueueTest<E> {
+
+    Queue<Integer> queue = new Queue<Integer>();
     
-    Test<E> queuelist = new Test<E>();
+    @Test
+    public void testEnqueue() {
+        queue.enqueue(1);
+        queue.enqueue(2);
 
-    Queue<Integer> queue = new Test<>();
+        Assertions.assertEquals(1, queue.first());
+        Assertions.assertEquals(2, queue.last());
+    }
 
-    //queue.enqueue();
+    @Test
+    public void testDequeue() {
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+
+        queue.dequeue();
+
+        Assertions.assertEquals(2, queue.first());
+        Assertions.assertEquals(3, queue.last());
+    }
+
+    @Test
+    public void test1000() {
+        for (int i = 0; i < 1000; i++) {
+            queue.enqueue(i);
+        }
+        queue.dequeue();
+        Assertions.assertEquals(999, queue.size());
+    }
+
+    @Test
+    public void testEmpty() {
+        Assertions.assertEquals(true, queue.isEmpty());
+
+        queue.enqueue(0);
+        Assertions.assertEquals(false, queue.isEmpty());
+
+        queue.dequeue();
+        Assertions.assertEquals(true, queue.isEmpty());
+    }
+
+
+
 
 }
