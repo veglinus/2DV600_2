4b27df6cff09aa098c595e8374bd2d62127b5656
Linus <linushvenfelt@gmail.com>
2021-10-06 15:18:22 +0200

    Queue

--- Exercise4/Queue.java
+++ Exercise4/Queue.java
@@ -1,10 +1,93 @@
 package Exercise4;
+import java.util.Iterator;
 
-public interface Queue<E> extends Iterable<E> {
-    int size(); // current queue size
-    boolean isEmpty(); // true if queue is empty
-    void enqueue(E element); // add element at end of queue
-    E dequeue(); // return and remove first element.
-    E first(); // return (without removing) first element
-    E last(); // return (without removing) last element
+class Queue<E> implements QueueInterface<E> {
+
+    Node<E> head = null;
+    Node<E> tail = null;
+    int size = 0;
+
+    public Iterator<E> iterator() {
+        return new ListIterator();
+    }
+
+    class ListIterator implements Iterator<E> {
+
+        Node<E> node = head;
+
+        @Override
+        public boolean hasNext() {
+            if (node != null) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public E next() {
+            E returnvalue = node.getValue();
+            node = node.next;
+
+            return returnvalue;
+        }
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public boolean isEmpty() {
+        if (this.size != 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void enqueue(E element) {
+
+        Node<E> newnode = new Node<E>(element, null);
+
+        if (head == null) {
+            head = newnode;
+            tail = newnode;
+        } else {
+            tail.next = newnode;
+            tail = tail.next;
+        }
+        size++;
+    }
+
+    public E dequeue() {
+        var returnvalue = first();
+
+        head = head.next;
+        size--;
+
+        return returnvalue;
+    }
+
+    public E first() {
+        return head.value;
+    }
+
+    public E last() {
+        return tail.value;
+    }
+
+    private class Node<E> {
+        E value;
+        Node<E> next = null;
+
+        Node(E value, Node<E> next) {
+            this.value = value;
+            this.next = next;
+        }
+
+        public E getValue() {
+            return this.value;
+        }
+    }
+
 }
